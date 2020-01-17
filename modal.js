371822072db74da7343f3a635abd318ea862be06
Mike Zarra <mkzarra@Mikes-MacBook-Air.local>
2020-01-17 14:54:35 -0500

    introduces comments that explain the code

--- modal.js
+++ modal.js
@@ -1,20 +1,21 @@
 var backdrop = document.getElementById('backdrop');
-var modal = document.getElementById('modal');
+var myModal = document.getElementById('modal');
 var closeButton = document.getElementById('close_button');
 var open = document.getElementById('open');
 
-backdrop.addEventListener('click', hideModal);
-closeButton.addEventListener('click', hideModal);
-open.addEventListener('click', () => showModal(modal));
+backdrop.addEventListener('click', hideModal); // this and the line below pass a reference to the hideModal function which will be executed when element is clicked
+closeButton.addEventListener('click', hideModal); 
+open.addEventListener('click', () => showModal(myModal)); // this arrow function prevents the function call from being immediately executed. This implementation is used here so that showModal can receive an arguement.
 
-function showModal(m) {
+function showModal(modal) {
+  // modal is a variable that can be used on whichever modal is passed through this function
   backdrop.style.display = 'block';
-  m.style.display = 'block';
+  modal.style.display = 'block';
   console.log('open');
 }
 
 function hideModal() {
   backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  myModal.style.display = 'none';
   console.log('close');
 }
